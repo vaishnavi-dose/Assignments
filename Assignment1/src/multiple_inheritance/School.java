79e7a19e90712f1ece94bb0f716c4f33c7c6747c
vaishnavi-dose <114053692+vaishnavi-dose@users.noreply.github.com>
2022-09-27 21:43:59 +0530

    commit

--- Assignment1/src/multiple_inheritance/School.java
+++ Assignment1/src/multiple_inheritance/School.java
@@ -1,6 +1,7 @@
 package multiple_inheritance;
 
 public interface School {
+	
 	void stud_reg_no();
 	void class_name();
 	void division();
--- Assignment1/src/multiple_inheritance/St_arnolds.java
+++ Assignment1/src/multiple_inheritance/St_arnolds.java
@@ -2,22 +2,21 @@ package multiple_inheritance;
 
 public class St_arnolds implements School {
 
-	@Override
-	public void stud_reg_no() {
-		// TODO Auto-generated method stub
-		System.out.println("Student Admission No");
-	}
+		@Override
+		public void stud_reg_no() {
+			// TODO Auto-generated method stub
+			System.out.println("Student Admission No");
+		}
 
-	@Override
-	public void class_name() {
-		// TODO Auto-generated method stub
-		System.out.println("Student Name");
-	}
+		@Override
+		public void class_name() {
+			// TODO Auto-generated method stub
+			System.out.println("Student Name");
+		}
 
-	@Override
-	public void division() {
-		// TODO Auto-generated method stub
-		System.out.println("Student Division");
-	}
-
+		@Override
+		public void division() {
+			// TODO Auto-generated method stub
+			System.out.println("Student Division");
+		}
 }
