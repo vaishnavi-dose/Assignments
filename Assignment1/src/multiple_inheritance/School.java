17ac74ab4ef98cc2482a1b3ad1de9719edca3926
vaishnavi-dose <114053692+vaishnavi-dose@users.noreply.github.com>
2022-09-27 20:08:46 +0530

    commit Multiple inheritance by interface

---
+++ Assignment1/src/multiple_inheritance/School.java
@@ -0,0 +1,8 @@
+package multiple_inheritance;
+
+public interface School {
+	void stud_reg_no();
+	void class_name();
+	void division();
+
+}
---
+++ Assignment1/src/multiple_inheritance/St_arnolds.java
@@ -0,0 +1,23 @@
+package multiple_inheritance;
+
+public class St_arnolds implements School {
+
+	@Override
+	public void stud_reg_no() {
+		// TODO Auto-generated method stub
+		System.out.println("Student Admission No");
+	}
+
+	@Override
+	public void class_name() {
+		// TODO Auto-generated method stub
+		System.out.println("Student Name");
+	}
+
+	@Override
+	public void division() {
+		// TODO Auto-generated method stub
+		System.out.println("Student Division");
+	}
+
+}
